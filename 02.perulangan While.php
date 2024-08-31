a1bb4f350ca7c79545d4dcac2c5f23b6e3f6283c
fikalaura27 <fkalractracitra@gmail.com>
2024-08-31 11:44:44 +0700

    02.perulangan.while.php

--- 02.perulangan While.php
+++ 02.perulangan While.php
@@ -1,10 +1,6 @@
-<?php 
+<?php
 
-$ulangi = 0;
-
-while($ulangi < 10){
-    echo "<p>Ini adalah perulangan ke-$ulangi</p>";
-    $ulangi++;
+for($i = 0; $i <= 10; $i++){
+    echo "<h2>Ini perulangan ke-$i</h2>";
 }
-
-echo "<p><strong>by: fika laura";
+?>
---
+++ 03..perulangan do while.php
@@ -0,0 +1,18 @@
+<?php 
+
+$ulangi = 6;
+
+do {
+    echo "<p>Ini adalah perulangan ke-$ulangi</p>";
+    $ulangi--;
+}while($ulangi < 0);
+
+$ulangi = 8;
+
+do {
+    echo "<p>Ini adalah perulangan ke-$ulangi</p>";
+    $ulangi--;
+} while ($ulangi >0);
+echo "<p><strong>by: fika laura";
+?>
+
