da52d873aa13f9324ceac6e2b850e58c25fa9b31
fikalaura27 <fkalractracitra@gmail.com>
2024-08-31 12:40:08 +0700

    11.for loop pada nilai tertentu.php

--- 11.For Loop Pada Nilai Tertentu.php
+++ 11.For Loop Pada Nilai Tertentu.php
@@ -1,8 +1,16 @@
 <?php
+
 for ($i = 1; $i <= 10; $i++) {
 	if ($i == 5) {
 		continue;
 	}
-    echo $i; // 1 2 3 4 6 7 8 9 10
+    echo "$i<p>"; // 1 2 3 4 6 7 8 9 10
 }
+
+for ($i = 1; $i <= 10; $i++) 
+	if ($i == 5) {
+		continue;
+	}
+    echo "$i<hr>"; // 1 2 3 4 6 7 8 9 10 11 12 13 14 15 
+
 echo "<p><strong>by: fika laura";
